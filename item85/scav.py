faf75e83b286998c604ebd1ab1dda735d2e7ff1b
Amanda <anakanishi@uchicago.edu>
2020-05-16 16:46:08 -0500

    I did stuff

--- item85/scav.py
+++ item85/scav.py
@@ -1,18 +1,38 @@
 import sys
 import os
 
-# fuck this bullshit
+#item 85
+
+def createFile(initial_command):
+	filename = initial_command.split(" ")
+	print(filename)
+	print(filename[2])
+	f = open(filename[2], "r")
+	text = f.read()
+	f.close()
+	text = text.replace("furret", "func")
+	text = text.replace("burmy", "package")
+	text = text.replace("impidimp", "import")
+	print(text)
+	f = open(filename[2].replace("pok", "go"), "w")
+	f.write(text)
+	f.close()
+
+
+	
 
 def parse(x):
 	x = x.replace("golem", "go")
 	x = x.replace("runerigus", "run")
+	x = x.replace("pok", "go")
 	print(x)
 	os.system(x)
 
 
-print('Welcome to pokemon-go. Run your command')
+print('Welcome to pokemon-go. To run your pokemon-go file, please run: golem runerigus [filename].pok')
 while(True):
-	x = input("$ ")
-	parse(x)
+	file = input("$ ")
+	createFile(file)
+	parse(file)
 
 
