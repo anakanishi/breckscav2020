4553ed06ac9818846064481b0de4390de9e85814
Amanda <anakanishi@uchicago.edu>
2020-05-16 17:26:10 -0500

    added the keywords to readme, cleaned up the main file a bit.

--- item85/scav.py
+++ item85/scav.py
@@ -3,36 +3,50 @@ import os
 
 #item 85
 
-def createFile(initial_command):
-	filename = initial_command.split(" ")
-	print(filename)
-	print(filename[2])
-	f = open(filename[2], "r")
-	text = f.read()
+def createGoFile(pok_filename):
+	"""
+	Reads in the contents of the .pok file
+	Writes the translated poklang into a .go file
+	Args:
+		pok_filename: name of the .pok file (should be in the same directory as this code)
+	"""
+	f = open(pok_filename, "r")
+	pok_text = f.read()
 	f.close()
+	go_text = parsePokLang(pok_text)
+	f = open(pok_filename.replace("pok", "go"), "w")
+	f.write(go_text)
+	f.close()
+
+def parsePokLang(text):
+	"""
+	Translates poklang into golang
+	Args:
+		text: poklang code
+	Returns:
+		text: golang code
+	"""
 	text = text.replace("furret", "func")
 	text = text.replace("burmy", "package")
 	text = text.replace("impidimp", "import")
-	print(text)
-	f = open(filename[2].replace("pok", "go"), "w")
-	f.write(text)
-	f.close()
+	return text
 
+def runPokLang(command, pokFile):
+	command = command.replace("golem", "go")
+	command = command.replace("runerigus", "run")
+	command = command.replace("pok", "go")
+	print(command)
+	os.system(command)
 
-	
-
-def parse(x):
-	x = x.replace("golem", "go")
-	x = x.replace("runerigus", "run")
-	x = x.replace("pok", "go")
-	print(x)
-	os.system(x)
+	# Delete the go file that was created
+	go_file = pokFile.replace("pok", "go")
+	os.remove(go_file)
 
 
 print('Welcome to pokemon-go. To run your pokemon-go file, please run: golem runerigus [filename].pok')
-while(True):
-	file = input("$ ")
-	createFile(file)
-	parse(file)
+command = input("$ ")
+filename = command.split(" ")[2]
+createGoFile(filename)
+runPokLang(command, filename)
 
 
